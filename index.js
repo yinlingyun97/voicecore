5d7476e8d5e8a3f470e8eab8783b24478f95e5d2
yinlingyun <yinlingyun178@163.com>
2021-09-06 22:52:18 +0800

    voice-core

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ navigator.getUserMedia =
  * @param {string} 参数名 appId  参数说明 讯飞实时语音转写接口appId
  * @param {string} 参数名 apiKey  参数说明 讯飞实时语音转写接口apiKey
  */
-export class voiceCore {
+export class VoiceCore {
   constructor(config, textData, appId, apiKey) {
     this.config = config;
     this.config.onMessage = (message) => {
@@ -166,7 +166,7 @@ export class voiceCore {
     }
   }
 
-  sendData = (buffer) => {
+  sendData (buffer) {
     var data = {
       command: 'transform',
       buffer: buffer,
@@ -265,7 +265,7 @@ export class voiceCore {
     }
   }
 
-  ArrayBufferToBase64 = (buffer) => {
+  ArrayBufferToBase64(buffer){
     var binary = '';
     const bytes = new Uint8Array(buffer);
     const len = bytes.byteLength;
